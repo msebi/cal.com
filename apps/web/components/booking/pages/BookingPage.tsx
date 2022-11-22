99179404f26a9f80a1b1403ece8bd1cc926e6c89
Udit Takkar <udit.07814802719@cse.mait.ac.in>
2022-11-22 14:29:25 +0530

    fix: null phone number bug and removed console log
    Signed-off-by: Udit Takkar <udit.07814802719@cse.mait.ac.in>

--- apps/web/components/booking/pages/BookingPage.tsx
+++ apps/web/components/booking/pages/BookingPage.tsx
@@ -301,14 +301,17 @@ const BookingPage = ({
   const bookEvent = (booking: BookingFormValues) => {
     const bookingCustomInputs = Object.keys(booking.customInputs || {}).map((inputId) => ({
       label: eventType.customInputs.find((input) => input.id === parseInt(inputId))?.label || "",
-      value: booking.customInputs && inputId in booking.customInputs ? booking.customInputs[inputId] : "",
+      value:
+        booking.customInputs && inputId in booking.customInputs && booking.customInputs[inputId]
+          ? booking.customInputs[inputId]
+          : "",
     }));
 
     // Check if custom input of type Phone number is valid
     const customInputsWithPhoneNumberType = eventType.customInputs.filter(
       (customInput) => customInput.type === EventTypeCustomInputType.PHONE
     );
-    console.log(customInputsWithPhoneNumberType);
+
     if (customInputsWithPhoneNumberType.length) {
       let isErrorFound = false;
       customInputsWithPhoneNumberType.forEach((customInput) => {
@@ -376,10 +379,7 @@ const BookingPage = ({
           attendeeAddress: booking.attendeeAddress,
         }),
         metadata,
-        customInputs: Object.keys(booking.customInputs || {}).map((inputId) => ({
-          label: eventType.customInputs.find((input) => input.id === parseInt(inputId))?.label || "",
-          value: booking.customInputs && inputId in booking.customInputs ? booking.customInputs[inputId] : "",
-        })),
+        customInputs: bookingCustomInputs,
         hasHashedBookingLink,
         hashedLink,
         smsReminderNumber:
@@ -407,10 +407,7 @@ const BookingPage = ({
           attendeeAddress: booking.attendeeAddress,
         }),
         metadata,
-        customInputs: Object.keys(booking.customInputs || {}).map((inputId) => ({
-          label: eventType.customInputs.find((input) => input.id === parseInt(inputId))?.label || "",
-          value: booking.customInputs && inputId in booking.customInputs ? booking.customInputs[inputId] : "",
-        })),
+        customInputs: bookingCustomInputs,
         hasHashedBookingLink,
         hashedLink,
         smsReminderNumber:
@@ -454,8 +451,6 @@ const BookingPage = ({
       ? ("rainbow" as Gate)
       : undefined,
   ];
-
-  console.log(bookingForm.formState);
 
   return (
     <Gates gates={gates} appData={rainbowAppData} dispatch={gateDispatcher}>
